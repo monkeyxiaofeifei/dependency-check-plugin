c8bb495a9da75bbc636c47d33746c86918190b56
GitHub Action <action@github.com>
2021-10-22 02:37:05 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "dependency-check-plugin"
---
+++ src/main/kotlin/com/github/monkeyxiaofeifei/dependencycheckplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.monkeyxiaofeifei.dependencycheckplugin.services
+
+import com.github.monkeyxiaofeifei.dependencycheckplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
